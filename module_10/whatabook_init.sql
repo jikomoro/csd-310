0111f7c67b47c37495d73ce728448fabae0c250f
Jae Dillon <103466697+jikomoro@users.noreply.github.com>
2023-08-08 17:40:31 -0700

    Create whatabook_init.sql

---
+++ module_10/whatabook_init.sql
@@ -0,0 +1,103 @@
+/*
+Title: whatabook_init.sql
+Author: Jae Dillon
+Date August 2023
+Description: WhatABook script
+*/
+
+-- creating database
+CREATE DATABASE whatabook;
+
+-- creating new user
+CREATE USER 'whatabook_user'@'localhost' IDENTIFIED WITH mysql_native_password BY 'MySQL8IsGreat!';
+
+-- creating tables
+CREATE TABLE user (
+  user_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(75) NOT NULL,
+  last_name VARCHAR(75) NOT NULL
+  );
+
+CREATE TABLE wishlist (
+  wishlist_id INT NOT NULL AUTO_INCREMENT,
+  user_id INT NOT NULL,
+  book_id INT NOT NULL,
+  PRIMARY KEY (wishlist_id),
+  CONSTRAINT fk_book
+  FOREIGN KEY (book_id) REFERENCES book(book_id),
+  CONSTRAINT fk_user
+  FOREIGN KEY (user_id) REFERENCES user(user_id)
+  );
+
+CREATE TABLE book (
+  book_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  book_name VARCHAR(200) NOT NULL,
+  book_details VARCHAR(500),
+  author VARCHAR(200) NOT NULL
+  );
+
+CREATE TABLE store (
+  store_id INT NOT NULL PRIMARY KEY,
+  locale VARCHAR(500) NOT NULL
+  );
+
+-- INSERT INTO store
+INSERT IGNORE INTO store(locale)
+  VALUES('123 Book St., Orange, CA 92000');
+
+-- INSERT INTO books
+INSERT IGNORE INTO book(book_name, author)
+  VALUES('A Game of Thrones', 'George R.R. Martin');
+
+INSERT IGNORE INTO book(book_name, author)
+  VALUES('A Clash of Kings', 'George R.R. Martin');
+
+INSERT IGNORE INTO book(book_name, author)
+  VALUES('The Magicians Nephew', 'C.S. Lewis');
+
+INSERT IGNORE INTO book(book_name, author)
+  VALUES('The Horse and His Boy', 'C.S. Lewis');
+
+INSERT IGNORE INTO book(book_name, author)
+  VALUES('Prince Caspian', 'C.S. Lewis');
+
+INSERT IGNORE INTO book(book_name, author)
+  VALUES('Revolution', 'Jennifer Donnelly');
+
+INSERT IGNORE INTO book(book_name, author)
+  VALUES('The Tea Rose', 'Jennifer Donnelly');
+
+INSERT IGNORE INTO book(book_name, author)
+  VALUES('A Thousand Splendid Suns', 'Khaled Hosseini');
+
+INSERT IGNORE INTO book(book_name, author)
+  VALUES('And the Mountains Echoed', 'Khaled Hosseini');
+
+-- INSERT INTO users
+INSERT INTO user(first_name, last_name)
+  VALUES('Eustace', 'Scrubb');
+
+INSERT INTO user(first_name, last_name)
+  VALUES('Ned', 'Stark');
+
+INSERT INTO user(first_name, last_name)
+  VALUES('Theon', 'Greyjoy');
+
+-- Insert wishlist items
+INSERT INTO wishlist(user_id, book_id)
+  VALUES(
+  (SELECT user_id FROM user WHERE first_name = 'Eustace'),
+  (SELECT book_id FROM book WHERE book_name = 'Prince Caspian')
+  );
+
+INSERT INTO wishlist(user_id, book_id)
+  VALUES(
+  (SELECT user_id FROM user WHERE first_name = 'Ned'),
+  (SELECT book_id FROM book WHERE book_name = 'A Game of Thrones')
+  );
+
+INSERT INTO wishlist(user_id, book_id)
+  VALUES(
+  (SELECT user_id FROM user WHERE first_name = 'Theon'),
+  (SELECT book_id FROM book WHERE book_name = 'A Thousand Splendid Suns')
+  );
